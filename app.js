b301e52ebf96c3df51b9406e9df26d1ed5867fd8
Eric Suescun <edsuescun@gmail.com>
2020-01-15 15:40:47 -0500

    Se envia un 200 cuando no hay nombre

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/maker/:nombre', (req, res) => {
+app.get('/makers/:nombre', (req, res) => {
 	let ans = '';
-	console.log('lo que hay: ' + req.params.nombre);
 	if(req.params.nombre.length !== 0) {
 		ans = req.params.nombre;
 		ans = ans.charAt(0).toUpperCase() + ans.slice(1);
-	}
-	if(req.param('nombre') === undefined) {
-		ans = 'desconocido';
+	} else {
+		res.sendStatus(200);
 	}
 	res.send(`<h1>Hola ${ans}!</h1>`);
 })
