9c678b5c87f63d5c22890e452a86dbd4ddb999c6
Eric Suescun <edsuescun@gmail.com>
2020-01-15 15:27:59 -0500

    Se implementa params pero se define un ruta para cuando no tenga params2

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.set('case sensitive routing', true);
-
-app.get('/', (req, res) => {
+app.get('/maker/:nombre', (req, res) => {
 	let ans = '';
-	if(req.query.nombre === undefined) {
-		ans = 'desconocido';
-	} else {
-		ans = req.query.nombre;
+	console.log('lo que hay: ' + req.params.nombre);
+	if(req.params.nombre.length !== 0) {
+		ans = req.params.nombre;
+		ans = ans.charAt(0).toUpperCase() + ans.slice(1);
 	}
-	res.send('<h1>Hola ' + ans + '!</h1>');
+	res.send(`<h1>Hola ${ans}!</h1>`);
+})
+
+app.get('/maker', (req, res) => {
+	res.send(`<h1>Hola desconocido!</h1>`);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
